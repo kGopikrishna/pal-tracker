771f9a194656da8e6b42e356a574fc2d2604b382
Gopi <kGopikrishna_kolleru@syntel.com>
2018-05-16 05:58:37 -0700

    Adding migrate Database 1

--- src/main/java/io.pivotal.pal.tracker/PalTrackerApplication.java
+++ src/main/java/io.pivotal.pal.tracker/PalTrackerApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
+
 @SpringBootApplication
 public class PalTrackerApplication {
 
